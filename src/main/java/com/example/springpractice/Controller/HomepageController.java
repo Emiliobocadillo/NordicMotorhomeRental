67e8df7ed37fcba1796540839159e5264b912778
Emil <Emilhoeeg@gmail.com>
2022-05-23 11:51:08 +0200

    changed Controller name

--- src/main/java/com/example/springpractice/Controller/HomepageController.java
+++ src/main/java/com/example/springpractice/Controller/HomepageController.java
@@ -5,12 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @org.springframework.stereotype.Controller
-public class Controller {
+public class HomepageController {
 
     @GetMapping
-    String getPeople(Model model){
+    String getHomepage(Model model){
         model.addAttribute("something", "this is coming from the controller");
-
         // thymeleaf knows that is has to look in the template folder for a file with the specified name
         return "index";
     }
