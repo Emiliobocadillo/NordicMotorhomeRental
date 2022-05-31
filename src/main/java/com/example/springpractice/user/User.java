b61afd4f1dd327326702bb10a65863f50b48300d
nhnahid1122 <nhnahid1122@gmail.com>
2022-05-31 13:50:49 +0200

    remove join email column and unnecessary things.

--- src/main/java/com/example/springpractice/user/User.java
+++ src/main/java/com/example/springpractice/user/User.java
@@ -13,8 +13,6 @@ public class User {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(updatable = false)
     private int id;
-    //change so it gets name from employee
-    /*private String email;*/
 
     private String email;
 
@@ -25,42 +23,4 @@ public class User {
     @Column(nullable = false)
     private  String password;
 
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "emp_email", referencedColumnName = "email")
-    private Employee employee;
-
-    /*
-    //Constructor
-    public User(){}
-
-    //Setters
-
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    //Getters
-
-    public String getName() {
-        return name;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-    to be deleted if lombok works like expected
-     */
 }
